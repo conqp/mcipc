d57a3eeba0b868bdf28afb28cb9302f4e023c6b5
Richard Neumann <mail@richard-neumann.de>
2018-03-16 20:25:52 +0100

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='mcipc',
+    author='Richard Neumann',
+    author_email='<mail at richard dash neumann dot de>',
+    packages=['mcipc', 'mcipc.rcon', 'mcipc.query'],
+    url='https://github.com/conqp/mcipc',
+    license='GPLv3',
+    description='A Minecraft server inter-process communication library.')
