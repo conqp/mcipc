44c5867740a61210219795d3ff041431a600572b
Richard Neumann <mail@richard-neumann.de>
2018-04-12 14:11:53 +0200

    Refactored splitting

--- mcipc/rcon/credentials.py
+++ mcipc/rcon/credentials.py
@@ -34,10 +34,8 @@ class Credentials(namedtuple('Credentials', ('host', 'port', 'passwd'))):
             InvalidCredentialsError(f'Not an integer: {port}.')
 
         try:
-            *passwd, host = host.split('@')
+            passwd, host = host.rsplit('@', maxsplit=1)
         except ValueError:
             passwd = None
-        else:
-            passwd = '@'.join(passwd)
 
         return cls(host, port, passwd)
