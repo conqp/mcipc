6e62adf557b3e52dc0c2d489d06f4aa247acf3bb
Richard Neumann <mail@richard-neumann.de>
2018-09-09 13:22:37 +0200

    Implemented Query client.

--- mcipc/query/__init__.py
+++ mcipc/query/__init__.py
@@ -1,1 +1,6 @@
 """Query API library."""
+
+from mcipc.client import Client
+
+
+__all__ = ['Client']
---
+++ mcipc/query/client.py
@@ -0,0 +1,52 @@
+"""Query client library."""
+
+from mcipc.common import BaseClient
+from mcipc.query.proto import basic_stats, full_stats, handshake
+
+
+__all__ = ['Client']
+
+
+class Client(BaseClient):
+    """A Query client."""
+
+    def __init__(self, host, port):
+        """Sets the challenge token."""
+        super().__init__(host, port)
+        self._challenge_token = None
+
+    def __enter__(self):
+        """Performs a handshake."""
+        super().__enter__()
+
+        if self._challenge_token is None:
+            self._challenge_token = self.handshake().challenge_token
+
+        return self
+
+    def communicate(self, packet, response_type=None):
+        """Sends and receives a packet."""
+        self._socket.send(bytes(packet))
+        response = self._socket.recv()
+
+        if response_type is not None:
+            return response_type.from_bytes(response)
+
+        return response
+
+    def handshake(self):
+        """Performs a handshake."""
+        request = handshake.Request.create()
+        return self.communicate(request, handshake.Response)
+
+    @property
+    def basic_stats(self):
+        """Returns basic stats"""
+        request = basic_stats.Request(self._challenge_token)
+        return self.communicate(request, basic_stats.BasicStats)
+
+    @property
+    def full_stats(self):
+        """Returns full stats"""
+        request = full_stats.Request(self._challenge_token)
+        return self.communicate(request, full_stats.FullStats)
