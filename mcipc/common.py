52758d1cdedefa703909253b1b4c0fae62767a5f
Richard Neumann <mail@richard-neumann.de>
2018-09-09 13:11:23 +0200

    Added common.py.

---
+++ mcipc/common.py
@@ -0,0 +1,40 @@
+"""Stuff, common to Query and RCON."""
+
+from socket import socket
+
+
+__all__ = ['BaseClient']
+
+
+class BaseClient:
+    """A basic client."""
+
+    def __init__(self, host: str, port: int):
+        """Sets host an port."""
+        self._socket = socket()
+        self.host = host
+        self.port = port
+
+    def __enter__(self):
+        """Conntects the socket."""
+        self._socket.__enter__()
+        self._socket.connect(self.socket)
+        return self
+
+    def __exit__(self, typ, value, traceback):
+        """Delegates to the underlying socket's exit method."""
+        self.close()
+        return self._socket.__exit__(typ, value, traceback)
+
+    @property
+    def socket(self) -> tuple:
+        """Returns a tuple of host and port."""
+        return (self.host, self.port)
+
+    def connect(self):
+        """Conntects to the RCON server."""
+        return self._socket.connect(self.socket)
+
+    def close(self):
+        """Disconnects from the RCON server."""
+        return self._socket.close()
