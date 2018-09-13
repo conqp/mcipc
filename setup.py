30b3b0bc96c3583ceb0edb7d9ea6484248cbf921
Richard Neumann <mail@richard-neumann.de>
2018-09-13 21:30:37 +0200

    Updated URL.

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setup(
         'mcipc.srvwrap.events',
         'mcipc.srvwrap.ipc'],
     scripts=['files/rconclt', 'files/rconshell', 'files/srvwrap-test'],
-    url='https://github.com/conqp/mcipc',
+    url='https://gitlab.com/coNQP/mcipc',
     license='GPLv3',
     description='A Minecraft server inter-process communication library.')
