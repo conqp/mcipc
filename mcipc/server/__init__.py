15251e76fa6ecba3993a02a32d84ec791705f3c3
Richard Neumann <mail@richard-neumann.de>
2019-06-11 21:57:38 +0200

    Refac.

--- mcipc/server/__init__.py
+++ mcipc/server/__init__.py
@@ -1,6 +1,6 @@
 """Mineicraft server protocol."""
 
-from mcipc.server.server import spawn
+from mcipc.server.server import StubServer
 
 
-__all__ = ['spawn']
+__all__ = ['StubServer']
--- mcipc/server/server.py
+++ mcipc/server/server.py
@@ -8,74 +8,77 @@ from mcipc.server.datatypes import VarInt
 from mcipc.server.enumerations import State
 
 
+__all__ = ['StubServer']
+
+
 LOGGER = getLogger(__file__)
 
 
-def get_slp_response(description, max_players=20, protocol=485):
-    """Returns an SLP response."""
+class StubServer:
+    """A stub minecraft server."""
 
-    json = {
-        'version': {
-            'name': '1.14.2',
-            'protocol': protocol
-        },
-        'players': {
-            'max': max_players,
-            'online': 0
-        },
-        'description': {
-            'text': description
+    def __init__(self, description, max_players=20, protocol=485):
+        """Description, max players and protocol information."""
+        self.description = description
+        self.max_players = max_players
+        self.protocol = protocol
+
+    @property
+    def slp_response(self):
+        """Returns an SLP response."""
+        json = {
+            'version': {
+                'name': '1.14.2',
+                'protocol': self.protocol
+            },
+            'players': {
+                'max': self.max_players,
+                'online': 0
+            },
+            'description': {
+                'text': self.description
+            }
         }
-    }
-    return SLPResponse(json)
+        return SLPResponse(json)
 
+    @staticmethod
+    def _perform_handshake(connection):
+        """Handle handshake requests."""
+        header = connection.recv(1)
+        size = VarInt.from_bytes(header)
+        LOGGER.debug('Read size: %s', size)
+        payload = connection.recv(size)
+        handshake = Handshake.from_bytes(payload)
+        LOGGER.debug('Got handshake: %s', handshake)
 
-def perform_handshake(connection):
-    """Handle handshake requests."""
+    def _perform_status(self, connection):
+        """Handles status requests."""
+        _ = connection.recv(1)  # Discard packet ID.
+        response = bytes(self.slp_response)
+        connection.send(response)
 
-    header = connection.recv(1)
-    size = VarInt.from_bytes(header)
-    LOGGER.debug('Read size: %s', size)
-    payload = connection.recv(size)
-    handshake = Handshake.from_bytes(payload)
-    LOGGER.debug('Got handshake: %s', handshake)
+    def _process(self, connection):
+        """Runs the connection processing."""
+        state = State.HANDSHAKE
 
+        with connection:
+            if state == State.HANDSHAKE:
+                LOGGER.debug('HANDSHAKE')
+                self._perform_handshake(connection)
+                self._perform_status(connection)
+                state = State.STATUS
+            elif state == State.STATUS:
+                LOGGER.debug('STATUS')
+                payload = connection.recv(1024)
+                LOGGER.debug('Payload: %s', payload)
 
-def perform_status(connection, slp_response):
-    """Handles status requests."""
+    def spawn(self, address, port):
+        """Spawns the server on the respective socket."""
+        with socket() as sock:
+            sock.bind((address, port))
+            sock.listen()
 
-    _ = connection.recv(1)  # Discard packet ID.
-    connection.send(bytes(slp_response))
-
-
-def process(connection, slp_response):
-    """Runs the connection processing."""
-
-    state = State.HANDSHAKE
-
-    with connection:
-        if state == State.HANDSHAKE:
-            LOGGER.debug('HANDSHAKE')
-            perform_handshake(connection)
-            perform_status(connection, slp_response)
-            state = State.STATUS
-        elif state == State.STATUS:
-            LOGGER.debug('STATUS')
-            payload = connection.recv(1024)
-            LOGGER.debug('Payload: %s', payload)
-
-
-def spawn(address, port, description, max_players=20, protocol=485):
-    """Spawns the server on the respective socket."""
-
-    slp_response = get_slp_response(
-        description, max_players=max_players, protocol=protocol)
-
-    with socket() as sock:
-        sock.bind((address, port))
-        sock.listen()
-
-        while True:
-            connection, address = sock.accept()
-            LOGGER.debug('New connection from: %s', address)
-            process(connection, slp_response)
+            while True:
+                connection, address = sock.accept()
+                LOGGER.debug('New connection from: %s', address)
+                self._process(connection)
