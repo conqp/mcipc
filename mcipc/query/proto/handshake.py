50eccf50e1ab4f4753eddcab3f7468e538617580
Richard Neumann <mail@richard-neumann.de>
2020-05-12 11:00:00 +0200

    Fixing challenge token is null-terminated string.

--- mcipc/query/proto/handshake.py
+++ mcipc/query/proto/handshake.py
@@ -43,7 +43,7 @@ class Response(NamedTuple):
         """Creates the packet from bytes."""
         type_ = Type.from_bytes(bytes_[0:1])
         session_id = int.from_bytes(bytes_[1:5], 'big', signed=True)
-        challenge_token = int.from_bytes(bytes_[5:-1], 'big', signed=True)
+        challenge_token = bytes_[5:-1].decode()
         return cls(type_, session_id, int(challenge_token))
 
     def to_json(self):
