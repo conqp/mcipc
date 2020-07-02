84f8a99c4158e44bac1de409152d259bbbc86a59
Richard Neumann <r.neumann@homeinfo.de>
2020-07-02 10:22:46 +0200

    Added further names to mcipc.rcon.__init__.py.

--- mcipc/rcon/__init__.py
+++ mcipc/rcon/__init__.py
@@ -1,6 +1,8 @@
 """RCON client library."""
 
 from mcipc.rcon.client import Client
+from mcipc.rcon.console import rconcmd
+from mcipc.rcon.exceptions import InvalidPacketStructure, WrongPassword
 
 
-__all__ = ['Client']
+__all__ = ['InvalidPacketStructure', 'WrongPassword', 'rconcmd', 'Client']
