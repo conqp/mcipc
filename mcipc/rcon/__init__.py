e5e995f290bdc0d559729e7fe85ce7002434a04b
Richard Neumann <r.neumann@homeinfo.de>
2020-09-08 12:03:18 +0200

    Added InvalidStructure to rcon.__init__.

--- mcipc/rcon/__init__.py
+++ mcipc/rcon/__init__.py
@@ -2,7 +2,15 @@
 
 from mcipc.rcon.client import Client
 from mcipc.rcon.console import rconcmd
-from mcipc.rcon.exceptions import InvalidPacketStructure, WrongPassword
+from mcipc.rcon.exceptions import InvalidPacketStructure
+from mcipc.rcon.exceptions import InvalidStructure
+from mcipc.rcon.exceptions import WrongPassword
 
 
-__all__ = ['InvalidPacketStructure', 'WrongPassword', 'rconcmd', 'Client']
+__all__ = [
+    'InvalidPacketStructure',
+    'InvalidStructure',
+    'WrongPassword',
+    'rconcmd',
+    'Client'
+]
