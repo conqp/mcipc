dc39d662d8baeba1ad8dd79349b14514e6fbaaa1
Richard Neumann <r.neumann@homeinfo.de>
2020-09-08 12:05:44 +0200

    Exposing exceptions, functions and clients in mcipc.__init__ now.

--- mcipc/__init__.py
+++ mcipc/__init__.py
@@ -1,1 +1,18 @@
 """An IPC library to communicate with Minecraft servers."""
+
+from mcipc.query import Client as QueryClient
+from mcipc.rcon import Client as RCONClient
+from mcipc.rcon import InvalidPacketStructure
+from mcipc.rcon import InvalidStructure
+from mcipc.rcon import WrongPassword
+from mcipc.rcon import rconcmd
+
+
+__all__ = [
+    'InvalidPacketStructure',
+    'InvalidStructure',
+    'WrongPassword',
+    'rconcmd',
+    'QueryClient',
+    'RCONClient'
+]
