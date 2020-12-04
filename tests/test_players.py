5cb952b02c86c2e21a36244e2784962f3a50d519
Richard Neumann <r.neumann@homeinfo.de>
2020-12-04 16:00:14 +0100

    Added tests for Paper servers.

--- tests/test_players.py
+++ tests/test_players.py
@@ -1,6 +1,6 @@
 """Tests players parsing."""
 
-from random import choices, randint
+from random import choice, choices, randint
 from string import ascii_letters, digits
 from typing import Iterable, NamedTuple
 
@@ -10,6 +10,12 @@ from mcipc.rcon.datastructures.players import Players
 
 
 VANILLA_STR = 'There are {online} of a max of {max} players online: {names}'
+PAPER_STR = (
+    '§6Es sind §c{online}§6 von maximal §c{max}§6 Spielern online.{names}'
+)
+PAPER_PLAYER_TEMPLATE = '§6{prefix}§r: §4{name}{suffix}§f'
+PAPER_PLAYER_SUFFIX = '§r'
+PAPER_PREFIXES = ('dev', 'obsidian')
 
 
 class PlayersTestSetting(NamedTuple):
@@ -23,6 +29,30 @@ class PlayersTestSetting(NamedTuple):
         """Returns a string as returned by a vanilla server."""
         return VANILLA_STR.format(
             online=self.online, max=self.max, names=', '.join(self.names))
+
+    def get_paper_players(self):
+        """Yieds player names formatted like on a Paper server."""
+        if not self.names:
+            return
+
+        yield ''    # For initial newline.
+        *names, last = self.names
+
+        for name in names:
+            yield PAPER_PLAYER_TEMPLATE.format(
+                prefix=choice(PAPER_PREFIXES), name=name,
+                suffix=PAPER_PLAYER_SUFFIX
+            )
+
+        yield PAPER_PLAYER_TEMPLATE.format(
+            prefix=choice(PAPER_PREFIXES), name=last, suffix='')
+
+    def to_paper_str(self):
+        """Returns a string as returned by a Paper server.
+        See: https://github.com/conqp/mcipc/issues/13
+        """
+        names = '\n'.join(self.get_paper_players())
+        return PAPER_STR.format(online=self.online, max=self.max, names=names)
 
 
 def get_random_player_name(pool=ascii_letters+digits, minlen=1, maxlen=100):
@@ -46,11 +76,20 @@ def get_player_settings(count=100):
 class TestPlayers(TestCase):
     """Tests players parsing on a vanilla server."""
 
+    def assertPlayersEqual(self, one, other):   # pylint: disable=C0103
+        """Checks whether players tuples are considered equal."""
+        self.assertEqual(one.online, other.online)
+        self.assertEqual(one.max, other.max)
+        self.assertSequenceEqual(one.names, other.names)
+
     def test_vanilla(self):
         """Tests players from a vanilla server."""
+        for test_setting in get_player_settings():
+            players = Players.from_response(test_setting.to_vanilla_str())
+            self.assertPlayersEqual(test_setting, players)
 
-        for player_setting in get_player_settings():
-            players = Players.from_response(player_setting.to_vanilla_str())
-            self.assertEqual(players.online, player_setting.online)
-            self.assertEqual(players.max, player_setting.max)
-            self.assertSequenceEqual(players.names, player_setting.names)
+    def test_paper(self):
+        """Tests players from a paper server."""
+        for test_setting in get_player_settings():
+            players = Players.from_response(test_setting.to_paper_str())
+            self.assertPlayersEqual(test_setting, players)
