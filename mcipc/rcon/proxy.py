8d42b59c2f7e9fe9f8d606a97abd3cba359de2d6
Richard Neumann <mail@richard-neumann.de>
2020-12-19 13:57:35 +0100

    Updated proxy.

--- mcipc/rcon/proxy.py
+++ mcipc/rcon/proxy.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+from mcipc.rcon.functions import until_none
 from mcipc.rcon.proto import Client
 
 
@@ -16,11 +17,14 @@ class CommandProxy:     # pylint: disable=R0903
     def __init__(self, client: Client, *args: str):
         """Sets the client and arguments."""
         self._client = client
-        self._args = args
+        self._args = tuple(until_none(args))
+
+    def __repr__(self):
+        return f'{type(self).__name__}({self._client}, {self._args})'
 
     def _proxy(self, cls: type, *args: str) -> CommandProxy:
         """Returns a sub-proxy."""
-        return cls(self._client, *self._args, *args)
+        return cls(self._client, *self._args, *until_none(args))
 
     def _run(self, *args) -> str:
         """Runs the respective command."""
