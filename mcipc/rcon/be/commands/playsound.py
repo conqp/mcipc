d6184dc55d15848ab7bc8a818df6a357ce591b73
Richard Neumann <mail@richard-neumann.de>
2020-12-19 15:14:01 +0100

    Implemented playsound command for bedrock edition.

---
+++ mcipc/rcon/be/commands/playsound.py
@@ -0,0 +1,17 @@
+"""Implementation of the playsound command."""
+
+from mcipc.rcon.proto import Client
+from mcipc.rcon.types import Vec3
+
+
+__all__ = ['playsound']
+
+
+# pylint: disable=R0913
+def playsound(self: Client, sound: str, player: str = None,
+              position: Vec3 = None, volume: float = None,
+              minimum_volume: float = None) -> str:
+    """Plays a sound."""
+
+    return self.run('playsound', sound, player, position, volume,
+                    minimum_volume)
