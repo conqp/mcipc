86ae34d5839997f468cefc7f10b2bb830d7f5cba
Richard Neumann <mail@richard-neumann.de>
2020-12-19 13:56:37 +0100

    Updated bedrock commands.

--- mcipc/rcon/be/commands/__init__.py
+++ mcipc/rcon/be/commands/__init__.py
@@ -1,1 +1,22 @@
 """Commands for the Bedrock Edition."""
+
+from mcipc.rcon.be.commands.ability import ability
+from mcipc.rcon.be.commands.camerashake import camerashake
+from mcipc.rcon.be.commands.clear import clear
+from mcipc.rcon.be.commands.clearspawnpoint import clearspawnpoint
+from mcipc.rcon.be.commands.clone import clone
+from mcipc.rcon.be.commands.difficulty import difficulty
+from mcipc.rcon.be.commands.effect import effect
+from mcipc.rcon.be.commands.enchant import enchant
+from mcipc.rcon.be.commands.event import event
+from mcipc.rcon.be.commands.fill import fill
+from mcipc.rcon.be.commands.fog import fog
+from mcipc.rcon.be.commands.gamemode import gamemode
+from mcipc.rcon.be.commands.give import give
+from mcipc.rcon.be.commands.help import help    # pylint: disable=W0622
+from mcipc.rcon.be.commands.list import list    # pylint: disable=W0622
+from mcipc.rcon.be.commands.locate import locate
+from mcipc.rcon.be.commands.particle import particle
+from mcipc.rcon.be.commands.setworldspawn import setworldspawn
+from mcipc.rcon.be.commands.spawnpoint import spawnpoint
+from mcipc.rcon.be.commands.xp import xp
