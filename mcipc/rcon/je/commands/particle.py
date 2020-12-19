a56b23bbb9a8e8e61e6afa0a8287044a96af561c
Richard Neumann <mail@richard-neumann.de>
2020-12-19 13:58:31 +0100

    Implemented java edition particle command.

---
+++ mcipc/rcon/je/commands/particle.py
@@ -0,0 +1,28 @@
+"""Implementation of the particle command."""
+
+from mcipc.rcon.je.types import ParticleMode
+from mcipc.rcon.proto import Client
+from mcipc.rcon.types import Vec3
+
+
+__all__ = ['particle']
+
+
+# pylint: disable=R0913
+def particle(self: Client, name: str, speed: float, count: int,
+             parameters: str = None, pos: Vec3 = None, delta: Vec3 = None,
+             mode: ParticleMode = None, viewers: str = None) -> str:
+    """Creates the respective particles."""
+
+    args = ['particle', name]
+
+    if parameters is not None:
+        args.append(parameters)
+
+    if pos is not None:
+        args.append(pos)
+
+        if delta is not None:
+            args.append(delta)
+
+    return self.run(*args, speed, count, mode, viewers)
