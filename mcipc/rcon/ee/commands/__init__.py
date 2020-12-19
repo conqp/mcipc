84fd4b79d248d3c0c35fb594bc65e6326a18ba8b
Richard Neumann <mail@richard-neumann.de>
2020-12-19 13:57:55 +0100

    Updated education commands.

--- mcipc/rcon/ee/commands/__init__.py
+++ mcipc/rcon/ee/commands/__init__.py
@@ -1,1 +1,17 @@
 """Commands for the Education Edition."""
+
+from mcipc.rcon.ee.commands.agent import collect
+from mcipc.rcon.ee.commands.agent import createagent
+from mcipc.rcon.ee.commands.agent import destroy
+from mcipc.rcon.ee.commands.agent import detect
+from mcipc.rcon.ee.commands.agent import detectredstone
+from mcipc.rcon.ee.commands.agent import dropall
+from mcipc.rcon.ee.commands.agent import move
+from mcipc.rcon.ee.commands.agent import remove
+from mcipc.rcon.ee.commands.agent import tpagent
+from mcipc.rcon.ee.commands.agent import transfer
+from mcipc.rcon.ee.commands.agent import turn
+from mcipc.rcon.ee.commands.classroommode import classroommode
+from mcipc.rcon.ee.commands.code import code
+from mcipc.rcon.ee.commands.geteduclientinfo import geteduclientinfo
+from mcipc.rcon.ee.commands.immutableworld import immutableworld
