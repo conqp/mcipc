456def98bcc151c4a85e7dcd0e1c1396abfcbd26
Richard Neumann <mail@richard-neumann.de>
2020-12-19 15:10:54 +0100

    Implemented playsound command for java edition.

---
+++ mcipc/rcon/je/commands/playsound.py
@@ -0,0 +1,17 @@
+"""Implementation of the playsound command."""
+
+from mcipc.rcon.proto import Client
+from mcipc.rcon.types import Vec3
+
+
+__all__ = ['playsound']
+
+
+# pylint: disable=R0913
+def playsound(self: Client, sound: str, source: str, targets: str,
+              pos: Vec3 = None, volume: float = None, pitch: float = None,
+              min_volume: float = None) -> str:
+    """Plays a sound."""
+
+    return self.run('playsound', sound, source, targets, pos, volume, pitch,
+                    min_volume)
