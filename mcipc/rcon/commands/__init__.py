ab393f29746cd148667a7c6b1adb579be01878da
Richard Neumann <mail@richard-neumann.de>
2020-12-19 13:57:13 +0100

    Updated common commands.

--- mcipc/rcon/commands/__init__.py
+++ mcipc/rcon/commands/__init__.py
@@ -1,1 +1,10 @@
 """Commands for all Minecraft versions."""
+
+from mcipc.rcon.commands.chat import me, say, tell, tellraw, send_url
+from mcipc.rcon.commands.execute import execute
+from mcipc.rcon.commands.function import function
+from mcipc.rcon.commands.gamerule import gamerule
+from mcipc.rcon.commands.kick import kick
+from mcipc.rcon.commands.kill import kill
+from mcipc.rcon.commands.op import deop, op
+from mcipc.rcon.commands.teleport import teleport
