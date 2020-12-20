6bc2ae7d823d44f713dbd475c411b0dc4b4978c3
Richard Neumann <mail@richard-neumann.de>
2020-12-20 14:07:31 +0100

    Updated type for source in playsound for the java edition.

--- mcipc/rcon/je/commands/playsound.py
+++ mcipc/rcon/je/commands/playsound.py
@@ -1,6 +1,7 @@
 """Implementation of the playsound command."""
 
 from mcipc.rcon.client import Client
+from mcipc.rcon.je.types import SoundSource
 from mcipc.rcon.types import Vec3
 
 
@@ -8,7 +9,7 @@ __all__ = ['playsound']
 
 
 # pylint: disable=R0913
-def playsound(self: Client, sound: str, source: str, targets: str,
+def playsound(self: Client, sound: str, source: SoundSource, targets: str,
               pos: Vec3 = None, volume: float = None, pitch: float = None,
               min_volume: float = None) -> str:
     """Plays a sound."""
