4d0fa7f356f89257c2712ba522a8156644e64190
Richard Neumann <mail@richard-neumann.de>
2020-12-20 14:52:30 +0100

    Implemented common tag command.

---
+++ mcipc/rcon/commands/tag.py
@@ -0,0 +1,29 @@
+"""Implementation of the tag command."""
+
+from mcipc.rcon.client import Client
+from mcipc.rcon.proxy import CommandProxy
+
+
+__all__ = ['tag']
+
+
+class TagProxy(CommandProxy):
+    """Proxy for tag commands."""
+
+    def add(self, name: str) -> str:
+        """Adds a tag."""
+        return self._run('add', name)
+
+    def list(self) -> str:
+        """Lists tags."""
+        return self._run('list')
+
+    def remove(self, name: str) -> str:
+        """Removes a tag."""
+        return self._run('remove', name)
+
+
+def tag(self: Client, targets: str) -> TagProxy:
+    """Delegates to a command proxy."""
+
+    return TagProxy(self, 'tag', targets)
