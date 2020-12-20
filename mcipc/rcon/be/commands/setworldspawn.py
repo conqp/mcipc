f8bf065c1a62271f49e9da58de7c949cfbd9d05a
Richard Neumann <mail@richard-neumann.de>
2020-12-20 13:36:03 +0100

    Refactored setworldspawn commands.

--- mcipc/rcon/be/commands/setworldspawn.py
+++ mcipc/rcon/be/commands/setworldspawn.py
@@ -7,8 +7,7 @@ from mcipc.rcon.types import Vec3
 __all__ = ['setworldspawn']
 
 
-def setworldspawn(self: Client, position: Vec3 = None) -> str:
+def setworldspawn(self: Client, spawn_point: Vec3 = None) -> str:
     """Sets the world spawn."""
 
-    return self.run('setworldspawn', position)
-
+    return self.run('setworldspawn', spawn_point)
--- mcipc/rcon/je/commands/setworldspawn.py
+++ mcipc/rcon/je/commands/setworldspawn.py
@@ -7,8 +7,7 @@ from mcipc.rcon.types import Vec3
 __all__ = ['setworldspawn']
 
 
-def setworldspawn(self: Client, position: Vec3 = None,
-                  angle: float = None) -> str:
+def setworldspawn(self: Client, pos: Vec3 = None, angle: float = None) -> str:
     """Sets the world spawn."""
 
-    return self.run('setworldspawn', position, angle)
+    return self.run('setworldspawn', pos, angle)
