52c2ed54834c0af84f8853fd2b6d591c0b87bcc9
Richard Neumann <mail@richard-neumann.de>
2020-12-20 23:46:38 +0100

    Implemented Education Edition API.

--- mcipc/rcon/ee/__init__.py
+++ mcipc/rcon/ee/__init__.py
@@ -1,6 +1,48 @@
 """RCON implementation for the Education Edition server."""
 
+from mcipc.rcon.be.types import Biome
+from mcipc.rcon.be.types import Enchantment
+from mcipc.rcon.be.types import EntityEquipmentSlot
+from mcipc.rcon.be.types import FillType
+from mcipc.rcon.be.types import Location
+from mcipc.rcon.be.types import MatchMode
+from mcipc.rcon.be.types import Mirror
+from mcipc.rcon.be.types import MusicRepeatMode
+from mcipc.rcon.be.types import Operator
+from mcipc.rcon.be.types import Order
+from mcipc.rcon.be.types import RelativeFloat
+from mcipc.rcon.be.types import ReplaceMode
+from mcipc.rcon.be.types import RideRules
+from mcipc.rcon.be.types import Rotation
+from mcipc.rcon.be.types import SaveCommand
+from mcipc.rcon.be.types import Structure
+from mcipc.rcon.be.types import StructureAnimationMode
+from mcipc.rcon.be.types import StructureSaveMode
+from mcipc.rcon.be.types import TeleportRules
+from mcipc.rcon.be.types import TimeSpec
 from mcipc.rcon.ee.client import Client
 
 
-__all__ = ['Client']
+__all__ = [
+    'Biome',
+    'Client',
+    'Enchantment',
+    'EntityEquipmentSlot',
+    'FillType',
+    'Location',
+    'MatchMode',
+    'Mirror',
+    'MusicRepeatMode',
+    'Operator',
+    'Order',
+    'RelativeFloat',
+    'ReplaceMode',
+    'RideRules',
+    'Rotation',
+    'SaveCommand',
+    'Structure',
+    'StructureAnimationMode',
+    'StructureSaveMode',
+    'TeleportRules',
+    'TimeSpec'
+]
