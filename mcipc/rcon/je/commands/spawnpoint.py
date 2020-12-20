1148dc2c3002dc4578374a604bb2a755c8e56e5c
Richard Neumann <mail@richard-neumann.de>
2020-12-20 13:37:07 +0100

    Refactored the spawnpoint command for the java edition.

--- mcipc/rcon/je/commands/spawnpoint.py
+++ mcipc/rcon/je/commands/spawnpoint.py
@@ -7,8 +7,8 @@ from mcipc.rcon.types import Vec3
 __all__ = ['spawnpoint']
 
 
-def spawnpoint(self: Client, targets: str = None, position: Vec3 = None,
+def spawnpoint(self: Client, targets: str = None, pos: Vec3 = None,
                angle: float = None) -> str:
     """Sets the spawn point for a player."""
 
-    return self.run('spawnpoint', targets, position, angle)
+    return self.run('spawnpoint', targets, pos, angle)
