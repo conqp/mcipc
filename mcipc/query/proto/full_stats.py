4ad6abd5968e186995100b1da18fc80489ddd78f
Richard Neumann <mail@richard-neumann.de>
2020-12-25 18:04:50 +0100

    Aligned inline comment.

--- mcipc/query/proto/full_stats.py
+++ mcipc/query/proto/full_stats.py
@@ -140,7 +140,7 @@ class FullStats(NamedTuple):
         session_id = BigEndianSignedInt32.read(file)
         _ = file.read(11)   # Discard padding.
         stats = read_stats(file)
-        _ = file.read(10)     # Discard padding.
+        _ = file.read(10)   # Discard padding.
         players = tuple(read_players(file))
         return cls(type_, session_id, *stats_from_dict(stats), players)
 
