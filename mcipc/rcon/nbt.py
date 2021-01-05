a1478c687ecd3b8f956d2c879a7b98c6ee6bf5e1
Richard Neumann <mail@richard-neumann.de>
2021-01-05 11:14:33 +0100

    Refactored NBT.

--- mcipc/rcon/nbt.py
+++ mcipc/rcon/nbt.py
@@ -1,12 +1,18 @@
 """Named binary tags."""
 
-from typing import Union
+from typing import Any, Dict, Union
 
 from mcipc.rcon.enumerations import Item, TargetSelector
 from mcipc.rcon.functions import stringify
 
 
 __all__ = ['NBT']
+
+
+def tags_to_str(tags: Dict[str, Any]) -> str:
+    """Returns the tags as a string."""
+
+    return ', '.join(f'{key}={stringify(val)}' for key, val in tags.items())
 
 
 class NBT:
@@ -21,11 +27,8 @@ class NBT:
 
     def __repr__(self):
         """Returns a str representation for eval()."""
-        items = ', '.join(f'{key}={val!r}' for key, val in self.tags.items())
-        return f'{type(self).__name__}({self.target!r}, {items})'
+        return f'{type(self).__name__}({self.target!r}, {self.tags!r})'
 
     def __str__(self):
         """Returns a str representation for RCON."""
-        items = ', '.join(
-            f'{key}={stringify(val)}' for key, val in self.tags.items())
-        return f'{self.target}[{items}]'
+        return f'{stringify(self.target)}[{tags_to_str(self.tags)}]'
