a17e8fe5a1c3bc6fc35bd083cccdc809b1e00a96
Richard Neumann <r.neumann@homeinfo.de>
2022-01-18 16:06:50 +0100

    Revert last change

--- mcipc/rcon/proxy.py
+++ mcipc/rcon/proxy.py
@@ -1,7 +1,6 @@
 """Command proxies."""
 
 from __future__ import annotations
-from typing import NamedTuple
 
 from mcipc.rcon.functions import until_none
 from mcipc.rcon.client import Client
@@ -10,16 +9,23 @@ from mcipc.rcon.client import Client
 __all__ = ['CommandProxy']
 
 
-class CommandProxy(NamedTuple):
+class CommandProxy:
     """A basic command proxy."""
 
-    client: Client
-    args: tuple[str] = ()
+    __slots__ = ('_client', '_args')
+
+    def __init__(self, client: Client, *args: str):
+        """Sets the client and arguments."""
+        self._client = client
+        self._args = tuple(until_none(args))
+
+    def __repr__(self):
+        return f'{type(self).__name__}({self._client}, {self._args})'
 
     def _proxy(self, cls: type, *args: str) -> CommandProxy:
         """Returns a sub-proxy."""
-        return cls(self.client, *self.args, *until_none(args))
+        return cls(self._client, *self._args, *until_none(args))
 
     def _run(self, *args) -> str:
         """Runs the respective command."""
-        return self.client.run(*self.args, *args)
+        return self._client.run(*self._args, *args)
