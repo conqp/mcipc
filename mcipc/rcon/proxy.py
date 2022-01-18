43ad1cd4e892d3af085ef1e79327ef2bf2ec3cf6
Richard Neumann <r.neumann@homeinfo.de>
2022-01-18 16:05:47 +0100

    Make CommandProxy a NamedTuple

--- mcipc/rcon/proxy.py
+++ mcipc/rcon/proxy.py
@@ -1,6 +1,7 @@
 """Command proxies."""
 
 from __future__ import annotations
+from typing import NamedTuple
 
 from mcipc.rcon.functions import until_none
 from mcipc.rcon.client import Client
@@ -9,23 +10,16 @@ from mcipc.rcon.client import Client
 __all__ = ['CommandProxy']
 
 
-class CommandProxy:     # pylint: disable=R0903
+class CommandProxy(NamedTuple):
     """A basic command proxy."""
 
-    __slots__ = ('_client', '_args')
-
-    def __init__(self, client: Client, *args: str):
-        """Sets the client and arguments."""
-        self._client = client
-        self._args = tuple(until_none(args))
-
-    def __repr__(self):
-        return f'{type(self).__name__}({self._client}, {self._args})'
+    client: Client
+    args: tuple[str] = ()
 
     def _proxy(self, cls: type, *args: str) -> CommandProxy:
         """Returns a sub-proxy."""
-        return cls(self._client, *self._args, *until_none(args))
+        return cls(self.client, *self.args, *until_none(args))
 
     def _run(self, *args) -> str:
         """Runs the respective command."""
-        return self._client.run(*self._args, *args)
+        return self.client.run(*self.args, *args)
