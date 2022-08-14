0b34125118d31e6c3931d3448c749df8d2e4e0c1
Richard Neumann <mail@richard-neumann.de>
2022-08-14 23:50:23 +0200

    Set max_packet_size

--- mcipc/rcon/client.py
+++ mcipc/rcon/client.py
@@ -8,7 +8,7 @@ from mcipc.rcon.functions import str_until_none
 __all__ = ['Client']
 
 
-class Client(source.Client):
+class Client(source.Client, max_packet_size=4096):
     """An RCON client for Minecraft."""
 
     def run(self, command: str, *arguments: str) -> str:
